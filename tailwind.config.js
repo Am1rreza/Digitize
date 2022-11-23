3d58e545f14e5c4d2ab3a5428003c13ff6912cda
Amirreza <amirreza.m13825@gmail.com>
2022-11-24 00:28:54 +0330

    improved filter section styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,10 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms")({
+      strategy: 'base', // only generate global styles
+      strategy: 'class', // only generate classes
+    }),
+  ],
 };
